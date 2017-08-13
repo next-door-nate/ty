673f566b13fb6d309b2839301a596b80c22d9fbe
Nate <Nate@Easy-Tobacco-40.hitronhub.home>
2017-08-13 13:20:28 -0500

    ty-app

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Happy Birthday Drew!</h2>
+          <h2>Hey Ty!</h2>
         </div>
         <p className="App-intro">
-          I&apos;m not sure what you&apos;re gonna use this for, but you have it now...
+          I haven&apos;t forgot about you... chill.
         </p>
       </div>
     );
